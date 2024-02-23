9e15e72e7b5542313f78b044d4f98158f033e5dc
Travis Heidelberger <70112002+TravisAlanH@users.noreply.github.com>
2024-02-23 15:47:18 -0500

    Security API Key env

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, setPersistence, browserLocalPersistence } from "firebase/auth";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyB5oDU-SJH87NFtiTb-XDmvk2B-L_QCOCg",
-  authDomain: "tcg-corner.firebaseapp.com",
-  projectId: "tcg-corner",
-  storageBucket: "tcg-corner.appspot.com",
-  messagingSenderId: "409104170287",
-  appId: "1:409104170287:web:c405665321b4420fad75da",
-  measurementId: "G-00Y9X43G83",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
