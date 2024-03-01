bde62a52d272b39c4a37446f4b501fbd9278bc01
Travis Heidelberger <70112002+TravisAlanH@users.noreply.github.com>
2024-03-01 17:37:12 -0500

    Selection Changes Commander Image

--- src/Comp/Pages/Games/GamesHome/MTGCommanderGameCode/MTGCommanderGameCode.js
+++ src/Comp/Pages/Games/GamesHome/MTGCommanderGameCode/MTGCommanderGameCode.js
@@ -2,13 +2,26 @@ import React from "react";
 import ShortUniqueId from "short-unique-id";
 import Axios from "axios";
 
+/**
+ * MTGCommanderGameCode Component for searching Magic: The Gathering Commander cards.
+ * Allows users to input a card name, searches for it using the Scryfall API,
+ * and displays a list of matching cards with their names, mana costs, and images.
+ */
 export default function MTGCommanderGameCode() {
+  // State variables
   const [searchedCard, setSearchedCard] = React.useState("");
   const [cardList, setCardList] = React.useState([]);
+  const [selectedCard, setSelectedCard] = React.useState(null); // New state for selected card
 
+  // Unique ID generator instance
   const uid = new ShortUniqueId();
   console.log(uid.randomUUID(6));
 
+  /**
+   * Handles the card search functionality.
+   * Utilizes the Scryfall API to search for Commander legal creature cards.
+   * Updates the state with the resulting card list.
+   */
   const handleCardSearch = () => {
     let searchItem = searchedCard.split(" ").join("+");
     Axios.get(
@@ -32,17 +45,30 @@ export default function MTGCommanderGameCode() {
     });
   };
 
+  /**
+   * Handles the selection of a card and updates the state.
+   * This function is called when an option is selected from the dropdown.
+   */
+  const handleCardSelection = (index) => {
+    setSelectedCard(cardList[index]);
+  };
+
+  // JSX rendering
   return (
     <div>
       <div className="flex flex-col border-2">
+        {/* Input for card search */}
         <input
           type="text"
           value={searchedCard}
           onChange={(e) => setSearchedCard(e.target.value)}
           className="border-2"
         />
+        {/* Button for initiating card search */}
         <button onClick={handleCardSearch}>Search</button>
-        <select className="border-2">
+        {/* Dropdown for selecting a commander */}
+        <select id="CommanderSelection" className="border-2" onChange={(e) => handleCardSelection(e.target.value)}>
+          <option value={0}>Select a Commander</option>
           {cardList.map((card, index) => {
             return (
               <option value={index} key={index}>
@@ -51,7 +77,8 @@ export default function MTGCommanderGameCode() {
             );
           })}
         </select>
-        <img src={cardList[0]?.imageURL} alt="Card" />
+        {/* Image of the selected commander */}
+        {selectedCard && <img src={selectedCard.imageURL} alt={selectedCard.name} style={{ maxWidth: "100%" }} />}
       </div>
     </div>
   );
